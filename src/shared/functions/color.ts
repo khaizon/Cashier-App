e0dc26dce6ca2993231d22453e886e6d35dc5d05
Kai Feng <kaifeng_toh@mymail.sutd.edu.sg>
2023-06-04 00:07:52 +0800

    deploy changes

--- src/shared/functions/color.ts
+++ src/shared/functions/color.ts
@@ -40,39 +40,6 @@ export const hexToBrighterHSL = (hexString: string, increaseLightness: number): 
   // Increase the lightness value
   l = Math.min(1, l + increaseLightness);
 
-  // Convert HSL back to RGB
-  let rOutput, gOutput, bOutput;
-  if (s === 0) {
-    rOutput = gOutput = bOutput = l;
-  } else {
-    const hueToRgb = (p: number, q: number, t: number): number => {
-      if (t < 0) t += 1;
-      if (t > 1) t -= 1;
-      if (t < 1 / 6) return p + (q - p) * 6 * t;
-      if (t < 1 / 2) return q;
-      if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
-      return p;
-    };
-
-    const q = l < 0.5 ? l * (1 + s) : l + s - l * s;
-    const p = 2 * l - q;
-    rOutput = hueToRgb(p, q, h + 1 / 3);
-    gOutput = hueToRgb(p, q, h);
-    bOutput = hueToRgb(p, q, h - 1 / 3);
-  }
-
-  // Convert RGB to hex
-  const rHex = Math.round(rOutput * 255)
-    .toString(16)
-    .padStart(2, '0');
-  const gHex = Math.round(gOutput * 255)
-    .toString(16)
-    .padStart(2, '0');
-  const bHex = Math.round(bOutput * 255)
-    .toString(16)
-    .padStart(2, '0');
-  const hexOutput = `#${rHex}${gHex}${bHex}`;
-
   // Return the HSL string
   const hslOutput = `hsl(${Math.round(h * 360)}, ${Math.round(s * 100)}%, ${Math.round(l * 100)}%)`;
   return hslOutput;
