4c972563d49fe27173da81604f8c08c6b9dca07a
Kai Feng <kaifeng_toh@mymail.sutd.edu.sg>
2023-06-07 13:53:57 +0800

    try pwa

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,40 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/Cashier-App/',
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['favicon.ico', 'apple-touch-icon.png', 'masked-icon.png'],
+      manifest: {
+        name: `Mao Mao's Cashier App`,
+        short_name: 'Cashier',
+        description: 'maomaothinks cashier app',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'pwa-64x64.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-192x192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'pwa-512x512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
   build: {
     emptyOutDir: true,
     outDir: './docs',
